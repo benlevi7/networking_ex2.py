6267eb52c5bb965ee00c1c6e0a42adad5d810daf
benle <benlevi7770528@gmail.com>
2021-11-11 00:51:30 -0800

    part1

--- server.py
+++ server.py
@@ -15,7 +15,10 @@ while True:
 
     data = client_socket.recv(100)
     print('Received: ', data)
+    client_socket.send(data.upper())
 
+    data = client_socket.recv(100)
+    print('Received: ', data)
     client_socket.send(data.upper())
 
     client_socket.close()
