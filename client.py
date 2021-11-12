32c10b574247785968e4361d67328f6819e2eb8f
benle <benlevi7770528@gmail.com>
2021-11-11 17:02:11 -0800

    start watch dog

--- client.py
+++ client.py
@@ -5,7 +5,11 @@ import socket
 import sys
 import os
 import time
+import watchdog
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
 
+"""
 IP = sys.argv[1]
 PORT = sys.argv[2]
 PATH = sys.argv[3]
@@ -39,7 +43,6 @@ def pushData():
 
 def pullData():
     numFiles = int.from_bytes(s.recv(1024), 'little')
-
     for indexFile in range(numFiles):
         relativePath = s.recv(1024).decode('utf8')
         fileName = relativePath.split('/')[-1]
@@ -62,6 +65,43 @@ if ID == 0:
 else:
     pullData()
 
+"""
+
+PATH = "/home/benle/PycharmProjects/networking_ex2.py/check"
+
+patterns = ["*"]
+ignore_patterns = None
+ignore_directories = False
+case_sensitive = True
+my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+def on_created(event):
+     print(f"{event.src_path} has been created!")
+
+def on_deleted(event):
+     print(f"Someone deleted {event.src_path}!")
 
 
-s.close()
+def on_moved(event):
+    print(f"someone moved {event.src_path} to {event.dest_path}")
+
+
+my_event_handler.on_created = on_created
+my_event_handler.on_deleted = on_deleted
+my_event_handler.on_moved = on_moved
+
+path = PATH
+go_recursively = True
+my_observer = Observer()
+my_observer.schedule(my_event_handler, path, recursive=go_recursively)
+
+my_observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    my_observer.stop()
+    my_observer.join()
+
+
+#s.close()
